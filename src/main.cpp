6bfdf8430e8ff2dce43dd24dad8a9b32c2ea6918
researcher2312 <miosz.segedyn@pl.abb.com>
2022-08-22 13:29:19 +0200

    added polling for user quit

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 int main(int argc, char* args[])
 {
 	std::string file_name {"../test.bmp"};
+	//Main loop flag
+	bool quit = false;
+	//Event handler
+	SDL_Event e;
     //Start up SDL and create window
     if (!init()) {
         printf("Failed to initialize!\n");
@@ -15,16 +19,21 @@ int main(int argc, char* args[])
         if(!loadMedia(file_name)) {
             printf("Failed to load media!\n");
         }
-        else{
-            //Apply the image
-            SDL_BlitSurface(gHelloWorld,NULL,gScreenSurface,NULL);
-			 //Update the surface
-            SDL_UpdateWindowSurface(gWindow);
-			            //Wait two seconds
-            SDL_Delay(2000);
-        }
     }
-
+	//While application is running
+	while(!quit) {
+	//Handle events on queue
+		while(SDL_PollEvent(&e) != 0) {
+			//User requests quit
+			if(e.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+		//Apply the image
+		SDL_BlitSurface(gHelloWorld,NULL,gScreenSurface,NULL );
+		//Update the surface
+		SDL_UpdateWindowSurface( gWindow );
+	}
     //Free resources and close SDL
     close();
 
