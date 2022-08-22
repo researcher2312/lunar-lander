8f15474abe52f38f5c61318d68c7e196b8d35161
researcher2312 <miosz.segedyn@pl.abb.com>
2022-08-22 13:28:58 +0200

    changed window name

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -24,7 +24,7 @@ bool init()
     }
     else{
         // Create window
-        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        gWindow = SDL_CreateWindow("Lunar Lander", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if (gWindow == NULL){
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
             success = false;
