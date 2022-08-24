81442c0a6ec4d2d17497917239a3c4784d585e4f
researcher2312 <miosz.segedyn@pl.abb.com>
2022-08-24 15:00:59 +0200

    added direct texture loading from file

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -78,20 +78,9 @@ SDL_Texture* loadTexture(std::string path)
     //The final texture
     SDL_Texture* newTexture = nullptr;
 
-    //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
-    if (loadedSurface == NULL){
-        printf("Unable to load image %s! SDL_image Error: %s\n",path.c_str(),IMG_GetError());
-    }
-    else {
-        //Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface(gRenderer,loadedSurface);
-        if (newTexture == NULL) {
-            printf("Unable to create texture from %s! SDL Error: %s\n",path.c_str(),SDL_GetError());
-        }
-
-        //Get rid of old loaded surface
-        SDL_FreeSurface(loadedSurface);
+    newTexture = IMG_LoadTexture(gRenderer,path.c_str());
+    if (newTexture == NULL) {
+        printf("Unable to create texture from %s! SDL Error: %s\n",path.c_str(),SDL_GetError());
     }
 
     return newTexture;
