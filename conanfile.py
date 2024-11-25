78e28b43cd56e1e1aba74341c2ef1e0a13f1929f
researcher2312 <researcher2312@gmail.com>
2024-11-25 16:44:50 +0100

    chore: update conanfile structure

--- conanfile.py
+++ conanfile.py
@@ -5,12 +5,17 @@ from conan.tools.cmake import cmake_layout
 
 
 class LunarLander(ConanFile):
+    name = "lunar-lander"
+    version = "0.1"
+    license = "GPL-3.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
+    default_options = {"libalsa*:shared": True}
 
     def requirements(self):
-        self.requires("sdl/2.30.3", force=True)
+        self.requires("sdl/2.30.9", force=True)
         self.requires("sdl_ttf/2.22.0")
 
     def layout(self):
         cmake_layout(self)
+
